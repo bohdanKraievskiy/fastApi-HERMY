bcd7242373d7c7c295b4c913088c9a36e0e4ea59
bohdanKraievskiy <bogdan.1233211233213@gmail.com>
2024-09-11 00:39:53 +0300

    newdb

--- server/database.py
+++ server/database.py
@@ -1,17 +1,29 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine,AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345678d@localhost/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://myuser:12345678@38.180.72.195:5432/mydb"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+async_session = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
 
 Base = declarative_base()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+async def init_db():
+    async with engine.begin() as conn:
+        # Створюємо всі таблиці
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def get_db():
+    async with async_session() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
--- server/main.py
+++ server/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from server.routers import user_router, task_router
-
+from .database import init_db
 app = FastAPI()
+
+@app.on_event("startup")
+async def on_startup():
+    await init_db()
 
 app.include_router(user_router.router, prefix="/users", tags=["Users"])
 app.include_router(task_router.router, prefix="/tasks", tags=["Tasks"])
