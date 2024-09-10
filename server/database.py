36be319725195ceab00cb661b640f00318312055
bohdanKraievskiy <bogdan.1233211233213@gmail.com>
2024-09-11 00:55:36 +0300

    newdb

--- server/database.py
+++ server/database.py
@@ -1,10 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine,AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://myuser:12345678@38.180.72.195:5432/mydb"
-
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+from decouple import config
+DATABASE_URL = config('SQLALCHEMY_DATABASE_URL')
+engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 async_session = sessionmaker(
